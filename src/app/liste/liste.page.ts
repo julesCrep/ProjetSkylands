90a194ebba884a5a2d4156329a1673071bfa72a5
crepelliere <crepellierej@outlook.fr>
2024-07-04 15:42:07 +0200

    création debut page liste

--- src/app/liste/liste.page.ts
+++ src/app/liste/liste.page.ts
@@ -1,20 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonRow, IonCol } from '@ionic/angular/standalone';
+import { Skylander } from '../model/skylander/skylander';
 
 @Component({
   selector: 'app-liste',
   templateUrl: './liste.page.html',
   styleUrls: ['./liste.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonRow, IonCol]
 })
 export class ListePage implements OnInit {
 
+  listeSkylanders : Skylander[] = [];
+
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() 
+  {
+    this.listeSkylanders.push(
+      new Skylander(
+        "Spyro",
+        "Dragon",
+        "Magie",
+        "Skylander",
+        "https://m.media-amazon.com/images/I/81L4HOmXLKL._AC_UF1000,1000_QL80_.jpg"
+      )
+    );
+
+    this.listeSkylanders.push(
+      new Skylander(
+        "Tree Rex",
+        "Un arbre",
+        "Vie",
+        "Géant",
+        "https://i.ebayimg.com/images/g/-SUAAOSwhpxidVb-/s-l400.png"
+      )
+    )
+
+    this.listeSkylanders.push(
+      new Skylander(
+        "Nitro Magna",
+        "Un robot",
+        "Tech",
+        "Swap_Force",
+        "https://thumbs.coleka.com/media/item/20160205/skylanders-nitro-magna-charge.webp"
+      )
+    )
+
+    this.listeSkylanders.push(
+      new Skylander(
+        "Trigger Happy",
+        "Quelque chose ?",
+        "Tech",
+        "Swap_Force",
+        "https://m.media-amazon.com/images/I/71E3w9OQRyL._AC_UF1000,1000_QL80_.jpg"
+      )
+    )
   }
 
 }
--- src/app/model/skylander/skylander.ts
+++ src/app/model/skylander/skylander.ts
@@ -8,4 +8,88 @@ export class Skylander {
     element : Element = Element.Air;
     role : Role = Role.Skylander;
     image : string = "";
+
+    constructor(
+        nom : string,
+        espece : string,
+        element : string,
+        role : string,
+        image : string
+    ){
+        this.nom = nom;
+        this.espece = espece;
+        this.element = this.determinerElement(element);
+        this.role = this.determinerRole(role);
+        this.image = image;
+    }
+
+    determinerElement(element : string) : Element{
+
+        switch ( element ) {
+            case "Eau":
+                return Element.Eau
+
+            case "Tech":
+                return Element.Tech
+
+            case "Terre":
+                return Element.Terre
+
+            case "Magie":
+                return Element.Magie
+
+            case "Feu":
+                return Element.Feu
+
+            case "Mort_Vivant":
+                return Element.Mort_Vivant
+
+            case "Vie":
+                return Element.Vie
+
+            case "Air":
+                return Element.Air
+
+            case "Lumiere":
+                return Element.Lumiere
+
+            case "Ténèbre":
+                return Element.Ténèbre
+
+            default: 
+                return Element.Eau
+         }
+    }
+
+    determinerRole(role : string) : Role
+    {
+        switch ( role ) {
+            case "Skylander":
+                return Role.Skylander
+
+            case "Senseï":
+                return Role.Senseï
+
+            case "Maraudeur":
+                return Role.Maraudeur
+
+            case "SuperChargers":
+                return Role.SuperChargers
+
+            case "Géant":
+                return Role.Géant
+
+            case "Swap_Force":
+                return Role.Swap_Force
+
+            case "Trap_Masters":
+                return Role.Trap_Masters
+
+            case "Mini_Skylanders":
+                return Role.Mini_Skylanders
+
+            default: 
+                return Role.Skylander
+         }
+    }
 }
